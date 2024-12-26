8acf12d6dcddc86cb5d5ad7c624442441ab81916
kishan.ganguli.tpr <kishan.ganguli.tpr@pwc.com>
2024-12-26 15:38:41 +0530

    registerd models

--- fileshareapp/models.py
+++ fileshareapp/models.py
@@ -8,7 +8,9 @@ class Folder(models.Model):
     uid = models.UUIDField(primary_key=True, editable= False, default=uuid.uuid4)
     created_date = models.DateTimeField(auto_now=True)
     updated_date = models.DateTimeField(auto_now=True )
-
+    def __str__(self):
+        return f"{str(self.pk)} - {self.created_date.date()}"
+    
 def get_upload_folder_path(instance , filename):
     return os.path.join(str(instance.folder.uid), filename)
 
@@ -17,3 +19,6 @@ class Files(models.Model):
     file = models.FileField(upload_to=get_upload_folder_path)
     created_date = models.DateTimeField(auto_now=True)
     updated_date = models.DateTimeField(auto_now=True )
+
+    def __str__(self):
+        return f"{str(self.pk)} - {self.folder}"
